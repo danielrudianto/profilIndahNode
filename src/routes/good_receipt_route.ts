105a821615e77b6a73815272a71264bfa37340a5
danielrudianto <danielrudianto12@gmail.com>
2022-06-03 18:04:11 +0700

    Update

--- src/routes/good_receipt_route.ts
+++ src/routes/good_receipt_route.ts
@@ -17,6 +17,9 @@ router.post("/", (req, res, next) => {
             company_id: company_id,
             supplier_id: supplier_id,
             created_by: req.body.userId,
+            is_confirm: true,
+            confirmed_by: req.body.userId,
+            confirmed_at: new Date()
         }
     }).then(result => {
         const good_receipt_items: any[] = [];
@@ -107,13 +110,162 @@ router.get("/archives", (req, res, next) => {
 })
 
 router.get("/archives/:year", (req, res, next) => {
+    const year = parseInt(req.params.year);
+    const count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    prisma.$queryRaw`SELECT COUNT(good_receipt_code.id) AS count, MONTH(good_receipt_code.date) AS month FROM good_receipt_code WHERE YEAR(good_receipt_code.date) = ${year} GROUP BY MONTH(good_receipt_code.date)`.then(counts => {
+        (counts as any[]).forEach(x => {
+            const month = x.month;
+            const num = x.count;
 
+            count[month - 1] = num;
+        });
+
+        res.status(200).send(count);
+    }).catch(error => {
+        res.status(500).send(error);
+    })
 })
 
 router.get("/archives/:year/:month", (req, res, next) => {
     const page = (!req.query.page) ? 1 : Math.max(parseInt(req.query.page.toString()), 1);
     const limit = parseInt(process.env.LIMIT!.toString());
     const offset = (page - 1) * limit;
+
+    const year = parseInt(req.params.year);
+    const month = parseInt(req.params.month);
+    const start_date = new Date(year, month - 1, 1, 0, 0, 0, 0);
+    const end_date = new Date(year, month, 1, 0, 0, 0, 0);
+    prisma.$transaction([
+        prisma.good_receipt_code.findMany({
+            where:{
+                AND: [
+                    {
+                        date: {
+                            gte: start_date
+                        }
+                    },
+                    {
+                        date: {
+                            lt: end_date
+                        }
+                    }
+                ]       
+            },
+            orderBy: {
+                date: "asc"
+            },
+            take: limit,
+            skip: offset,
+            select: {
+                name: true,
+                id: true,
+                supplier: {
+                    select: {
+                        name: true
+                    }
+                },
+                company: {
+                    select: {
+                        name: true
+                    }
+                },
+                date: true,
+                user_good_receipt_code_created_byTouser: {
+                    select: {
+                        name: true
+                    }
+                },
+                created_at: true,
+                is_delete: true,
+                is_confirm: true
+            }
+        }),
+        prisma.good_receipt_code.count({
+            where:{
+                AND: [
+                    {
+                        date: {
+                            gte: start_date
+                        }
+                    },
+                    {
+                        date: {
+                            lt: end_date
+                        }
+                    }
+                ]       
+            },
+        })
+    ]).then(result => {
+        res.status(200).send({
+            data: result[0],
+            count: result[1]
+        });
+    }).catch(error => {
+        res.status(500).send(error);
+    })
+});
+
+router.get("/:id", (req, res, next) => {
+    const id = parseInt(req.params.id);
+    prisma.good_receipt_code.findUnique({
+        where:{
+            id: id
+        },
+        select:{
+            name: true,
+            date: true,
+            user_good_receipt_code_created_byTouser: {
+                select: {
+                    name: true
+                }
+            },
+            created_at: true,
+            user_good_receipt_code_confirmed_byTouser: {
+                select: {
+                    name: true
+                }
+            },
+            confirmed_at: true,
+            is_confirm: true,
+            is_delete: true,
+            company: {
+                select: {
+                    name: true,
+                    address: true,
+                    npwp: true
+                }
+            },
+            supplier: {
+                select: {
+                    name: true,
+                    address: true,
+                    npwp: true
+                }
+            },
+            good_receipt: {
+                select: {
+                    item: {
+                        select: {
+                            reference: true,
+                            description: true
+                        }
+                    },
+                    quantity: true
+                }
+            },
+            purchase_invoice: {
+                select: {
+                    name: true,
+                    date: true
+                }
+            }
+        }
+    }).then(result => {
+        res.status(200).send(result);
+    }).catch(error => {
+        res.status(500).send(error);
+    })
 })
 
 export default router;
